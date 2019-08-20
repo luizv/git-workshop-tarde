b17bd8d0482b2f8e0f4b3e1d46782cd096a5ee30
André Tosin <andre_tosin@hotmail.com>
2019-08-20 16:18:15 -0300

    adiciona AndreViewController.swift e Andre.storyboard

---
+++ GitWorkshop/GitWorkshop/AndreViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AndreViewController.swift
+//  GitWorkshop
+//
+//  Created by Andre Tosin on 20/08/19.
+//  Copyright © 2019 Luiz Veloso. All rights reserved.
+//
+
+import UIKit
+
+class AndreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
